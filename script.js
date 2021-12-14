547657295902dbc68d45cb31c5ff5d974c992e34
Maurice Long <longmaurice255@gmail.com>
2021-12-13 20:37:32 -0800

    implemented smooth scrolling function

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 ///////////////////////////////////////
+//Selecting elements
+
 // Modal window
 
 const modal = document.querySelector('.modal');
@@ -8,7 +10,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +21,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -28,4 +30,38 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+
+//Event handler
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+
+  //Scrolling
+  //hard scroll
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  //smooth scroll
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  //modern syntax (very similar to wordpress)
+  section1.scrollIntoView({ behavior: 'smooth' });
+
+  //get current scroll x/y
+  //window.pageXOffset
+  //window.pageYOffset
+
+  //get viewport height/width
+  //document.documentElement.clientHeight
+  //document.documentELement.clientWidth
 });
